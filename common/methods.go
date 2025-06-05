474388bf7944a8bc2ccf25f111931b6aeb4ae7a1
Артемий Беляков (Москва) <a.belyakov@cloud.gcm>
2025-06-05 11:40:08 +0300

    Добавить методы обработки JSON 	modified:   common/methods.go 	modified:   go.mod 	new file:   go.sum 	modified:   version
    	modified:   common/methods.go
    	modified:   go.mod
    	new file:   go.sum
    	modified:   version

--- common/methods.go
+++ common/methods.go
@@ -1,13 +1,164 @@
 package common
 
 import (
+	"encoding/json"
 	"fmt"
 	"strings"
 
 	"github.com/av-belyakov/objectsthehiveformat/supportingfunctions"
+	"go.mongodb.org/mongo-driver/bson"
 )
 
 // ************* CustomFields **************
+
+// Get поле CustomFields
+func (fields *CustomFields) Get() CustomFields {
+	return *fields
+}
+
+// Set поле CustomFields
+func (fields *CustomFields) Set(v CustomFields) {
+	if *fields == nil {
+		*fields = make(CustomFields)
+	}
+
+	for key, value := range v {
+		(*fields)[key] = value
+	}
+}
+
+func (fields *CustomFields) UnmarshalJSON(data []byte) error {
+	type tmpCustomFieldType map[string]*json.RawMessage
+
+	list := map[string]string{
+		"attack-type":         "string",
+		"class-attack":        "string",
+		"event-source":        "string",
+		"external-letter":     "string",
+		"geoip":               "string",
+		"ncircc-class-attack": "string",
+		"ncircc-bulletin-id":  "string",
+		"inbox1":              "string",
+		"inner-letter":        "string",
+		"ir-name":             "string",
+		"id-soa":              "string",
+		"notification":        "string",
+		"sphere":              "string",
+		"state":               "string",
+		"report":              "string",
+		"first-time":          "date",
+		"last-time":           "date",
+		"b2mid":               "integer",
+		"is-incident":         "boolen",
+		"work-admin":          "boolen",
+	}
+
+	tmp := tmpCustomFieldType{}
+	if err := json.Unmarshal(data, &tmp); err != nil {
+		return err
+	}
+
+	newResult := make(CustomFields, len(tmp))
+	for k, v := range tmp {
+		name, ok := list[k]
+		if !ok {
+			continue
+		}
+
+		var field CustomerFields
+		switch name {
+		case "string":
+			field = &CustomFieldStringType{}
+
+		case "date":
+			field = &CustomFieldDateType{}
+
+		case "integer":
+			field = &CustomFieldIntegerType{}
+
+		case "boolen":
+			field = &CustomFieldBoolenType{}
+
+		}
+
+		if err := json.Unmarshal(*v, field); err != nil {
+			return err
+		}
+
+		newResult[k] = field
+	}
+
+	fields.Set(newResult)
+
+	return nil
+}
+
+func (fields *CustomFields) UnmarshalBSON(data []byte) error {
+	type tmpCustomFieldType map[string]*bson.Raw
+
+	list := map[string]string{
+		"attack-type":         "string",
+		"class-attack":        "string",
+		"event-source":        "string",
+		"external-letter":     "string",
+		"geoip":               "string",
+		"ncircc-class-attack": "string",
+		"ncircc-bulletin-id":  "string",
+		"inbox1":              "string",
+		"inner-letter":        "string",
+		"ir-name":             "string",
+		"id-soa":              "string",
+		"notification":        "string",
+		"sphere":              "string",
+		"state":               "string",
+		"report":              "string",
+		"first-time":          "date",
+		"last-time":           "date",
+		"b2mid":               "integer",
+		"is-incident":         "boolen",
+		"work-admin":          "boolen",
+	}
+
+	tmp := tmpCustomFieldType{}
+	if err := bson.Unmarshal(data, &tmp); err != nil {
+		return err
+	}
+
+	//newResult := make(map[string]CustomerFields, len(tmp.CustomFields))
+	newResult := make(CustomFields, len(tmp))
+	for k, v := range tmp {
+		name, ok := list[k]
+		if !ok {
+			continue
+		}
+
+		var field CustomerFields
+		switch name {
+		case "string":
+			field = &CustomFieldStringType{}
+
+		case "date":
+			field = &CustomFieldDateType{}
+
+		case "integer":
+			field = &CustomFieldIntegerType{}
+
+		case "boolen":
+			field = &CustomFieldBoolenType{}
+
+		}
+
+		if err := json.Unmarshal(*v, field); err != nil {
+			return err
+		}
+
+		newResult[k] = field
+	}
+
+	fields.Set(newResult)
+
+	return nil
+}
 
 // Get возвращает значения CustomFieldStringType, где 1 и 3 значение это
 // наименование поля
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/av-belyakov/objectsthehiveformat
 
 go 1.24.3
+
+require go.mongodb.org/mongo-driver v1.17.3
